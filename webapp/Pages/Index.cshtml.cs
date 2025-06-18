d586b59c0e64db0523145cbeec8a075635782bdb
Jose Ramon Alarcon <jose.alarcon.chaidez@accenture.com>
2025-06-18 11:28:05 -0600

    My commit

--- webapp/Pages/Index.cshtml.cs
+++ webapp/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ namespace webapp.Pages
 {
     public class IndexModel : PageModel
     {
+        // My first change
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
