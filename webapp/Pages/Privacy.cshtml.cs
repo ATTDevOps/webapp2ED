36dd7d2542f2a4736271b33fd4ace432f057dd4e
Jose Ramon Alarcon <jose.alarcon.chaidez@accenture.com>
2025-06-18 11:59:09 -0600

    Last comment

--- webapp/Pages/Privacy.cshtml.cs
+++ webapp/Pages/Privacy.cshtml.cs
@@ -5,6 +5,7 @@ namespace webapp.Pages
 {
     public class PrivacyModel : PageModel
     {
+        // My third change
         private readonly ILogger<PrivacyModel> _logger;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
